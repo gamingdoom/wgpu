1eed1dd3bd1a689fe24afd098545ab715a58e661
Pranay Sanghai <37276884+gamingdoom@users.noreply.github.com>
2025-05-20 23:19:53 -0700

    Update mod.rs

--- naga/src/back/glsl/mod.rs
+++ naga/src/back/glsl/mod.rs
@@ -4363,16 +4363,16 @@ impl<'a, W: Write> Writer<'a, W> {
             
             Expression::RayQueryGetIntersection { query, committed } => {
                 write!(self.out, "RayIntersection(")?;
-                write!(self.out, "rayQueryGetIntersectionTypeEXT({}, {}),", Baked(query), committed as u8)?;
-                write!(self.out, "rayQueryGetIntersectionTEXT({}, {}),", Baked(query), committed as u8)?;
-                write!(self.out, "rayQueryGetIntersectionInstanceCustomIndexEXT({}, {}),", Baked(query), committed as u8)?;
-                write!(self.out, "rayQueryGetIntersectionInstanceShaderBindingTableRecordOffsetEXT({}, {}),", Baked(query), committed as u8)?;
-                write!(self.out, "rayQueryGetIntersectionGeometryIndexEXT({}, {}),", Baked(query), committed as u8)?;
-                write!(self.out, "rayQueryGetIntersectionPrimitiveIndexEXT({}, {}),", Baked(query), committed as u8)?;
-                write!(self.out, "rayQueryGetIntersectionBarycentricsEXT({}, {}),", Baked(query), committed as u8)?;
-                write!(self.out, "rayQueryGetIntersectionFrontFaceEXT({}, {}),", Baked(query), committed as u8)?;
-                write!(self.out, "rayQueryGetIntersectionObjectToWorldEXT({}, {}),", Baked(query), committed as u8)?;
-                write!(self.out, "rayQueryGetIntersectionWorldToObjectEXT({}, {})", Baked(query), committed as u8)?;
+                write!(self.out, "rayQueryGetIntersectionTypeEXT({}, {}),", Baked(query), committed)?;
+                write!(self.out, "rayQueryGetIntersectionTEXT({}, {}),", Baked(query), committed)?;
+                write!(self.out, "rayQueryGetIntersectionInstanceCustomIndexEXT({}, {}),", Baked(query), committed)?;
+                write!(self.out, "rayQueryGetIntersectionInstanceShaderBindingTableRecordOffsetEXT({}, {}),", Baked(query), committed)?;
+                write!(self.out, "rayQueryGetIntersectionGeometryIndexEXT({}, {}),", Baked(query), committed)?;
+                write!(self.out, "rayQueryGetIntersectionPrimitiveIndexEXT({}, {}),", Baked(query), committed)?;
+                write!(self.out, "rayQueryGetIntersectionBarycentricsEXT({}, {}),", Baked(query), committed)?;
+                write!(self.out, "rayQueryGetIntersectionFrontFaceEXT({}, {}),", Baked(query), committed)?;
+                write!(self.out, "rayQueryGetIntersectionObjectToWorldEXT({}, {}),", Baked(query), committed)?;
+                write!(self.out, "rayQueryGetIntersectionWorldToObjectEXT({}, {})", Baked(query), committed)?;
                 write!(self.out, ")")?;
             }
 
